1b14db43c8e7c8f5c28a1230035e2fab8f9ecd4a
Katie Shanahan <keshanahan@gmail.com>
2019-03-19 21:45:32 -0400

    updated training prep

--- training_code/training.R
+++ training_code/training.R
@@ -6,6 +6,11 @@ source("preprocessing_code/calcpoints.R")
 library(tidyverse)
 library(mlr)
 
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
 bats_input <- readRDS("clean_data/retrobats2017.RDS")
 names(bats_input)
 
@@ -20,9 +25,20 @@ bats_mean <- bats_input %>%
          team.key.home, -team.key.away, -homescore, -awayscore, -B_G_DH, -B_G_PH, -B_G_PR, -F_1B_POS,
          -F_2B_POS, -F_3B_POS, -F_SS_POS, -F_OF_POS, -F_LF_POS, -F_CF_POS, -F_RF_POS, F_C_POS, -F_P_POS) %>%
   group_by(person.key) %>%
-  summarise_if(is.numeric, list(mean=mean, sd=sd), na.rm=TRUE)
+  summarise_if(is.numeric, funs(mean=mean), na.rm=TRUE)
 
-
+bats_sd <- bats_input %>%
+  select(-game.key, -game.date, -team.alignment, -team.key, -opponent.key, -slot, -seq,
+         team.key.home, -team.key.away, -homescore, -awayscore, -B_G_DH, -B_G_PH, -B_G_PR, -F_1B_POS,
+         -F_2B_POS, -F_3B_POS, -F_SS_POS, -F_OF_POS, -F_LF_POS, -F_CF_POS, -F_RF_POS, F_C_POS, -F_P_POS) %>%
+  group_by(person.key) %>%
+  summarise_if(is.numeric, funs(sd=sd), na.rm=TRUE)
 
 bats_mode <- bats_input %>%
-  select(person.key, )
+  select(person.key, team.key, slot) %>%
+  group_by(person.key) %>%
+  summarise_all(funs(mode=getmode))
+
+bats_all <- merge(bats_sum, bats_mean, by="person.key")
+bats_all <- merge(bats_all, bats_sd, by="person.key")
+bats_all <- merge(bats_all, bats_mode, by="person.key")
